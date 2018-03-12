2678bc698129d0848bc90cb6f4f4c8e7f08703de
poliakev <jpoliakevitch@gmail.com>
2018-03-12 06:13:53 -0600

    lots of comments but two working windows and passwords work :)

--- waste.py
+++ waste.py
@@ -35,13 +35,18 @@ class LoginFrame(Frame):
         self.label_username.grid(row = 0, sticky = E)
         self.label_password.grid(row = 1, sticky = E)
         self.entry_username.grid(row = 0, column = 1)
-        self.entry_password.grid(row = 1, column = 1)
+        self.entry_password.grid(row = 1, column = 1)      
         
         self.checkbox = Checkbutton(self, text='Keep me logged in')
         self.checkbox.grid(columnspan = 2)
 
-        self.logbtn = Button(self, text="Login", command =self._login_btn_clicked)
+        self.logbtn = Button(self, text = "Login", command = self._login_btn_clicked)
         self.logbtn.grid(columnspan = 2)
+        self.quit = Button(self, text = "QUIT", fg = "red", command = root.destroy)
+        self.quit.grid(columnspan = 2)
+
+
+        self.mycustomers = [] #maybe shouldn't be here o well
 
         self.pack()
 
@@ -68,7 +73,6 @@ class LoginFrame(Frame):
             if username == userLog[i]:
                 hashed_pwd = pbkdf2_hmac(hash_name, bytearray(password, 'ascii'), bytearray(salt, 'ascii'), iterations)
                 if hashed_pwd == userPassword[i]:
-                    print ('bruh')
                     self.operation_window(userRole[i], userID[i], userSupervisor[i])
                 else:
                     tm.showerror("Incorrect Password")
@@ -77,12 +81,94 @@ class LoginFrame(Frame):
         self.myID = ID
         self.myRole = role
         self.mySupervisor = supervisor
-        self.master.withdraw()
         self.newWindow = Toplevel(self.master)
         self.newWindow.wm_title("#%s menu" % self.myRole)
-        #l = lf.Label(t, text="some text")
-        #l.pack(side ="top", fill = "both", expand = True, padx = 100, pady = 100)
+
+        if role == 'account manager':
+            self.SelectCustomer = Button(self.newWindow, text = "Select Customer", command = self._select_cust_clicked())
+            self.SelectCustomer.grid(row = 0, column = 0)
+            #self.CreateNewAccount = Button(self.newWindow, text = "Create New Account", command = self._create_new_acc())
+            #self.CreateNewAccount.grid(row = 0, column = 1)
+            #self.CreateNewAccount.pack()
+            #self.CreateNewAgreement = Button(self.newWindow, text = "Create New Agreement", command = self._create_service_agr())
+            #self.CreateNewAgreement.grid(row = 1, column = 0)
+            #self.CreateNewAgreement.pack()
+            #self.CreateReport = Button(self.newWindow, text = "Create Report", command = self._create_report())
+            #self.CreateReport.grid(row = 1, column = 1)
+            #self.CreateNewReport.pack()
+
+            #move the following into select customer window with a withdraw()
+            #self.lableselectcust = Label(self.newWindow, text = "Enter master account number")
+            #self.inputmasternum = Entry(self.newWindow)
+            #self.lableselectcust.grid(row = 0, column = 1)
+            #self.inputmasternum.grid(row = 1, column = 1)
+            #self.selectcust = Button(self.newWindow, text = "Select", command = self._select_cust_clicked(self.myID))
+            #self.selectcust.grid(columnspan = 2)
+            self.returnbutton = Button(self.newWindow, text = "Return", command= self.newWindow.destroy)
+            self.returnbutton.grid(columnspan = 2)
         
+        #self.scrollbar = ScrollBar(self)
+        #self.scrollbar.pack(side=RIGHT, fill = 'y')
+       
+        #self.listbox = Listbox(self)
+        #self.listbox.pack() 
+        # test master acc # 87625036 for own23
+    def _create_new_acc(self):
+        #todo
+        print ('hi')
+
+    def _create_service_agr(self):
+        #todo
+        print ('you tired bro')
+
+    def _create_report(self):
+        #todo
+        print ('give up')
+
+    def _select_cust_clicked(self):
+        #self.custselect = Toplevel(self.newWindow)
+        #self.customer = Entry(self.custselect)
+        #self.customer.grid(columnspan = 2)
+
+        statement = ('select account_no from accounts where account_mgr = ?')
+        for row in waste.execute(statement, [self.myID]):
+            self.mycustomers.append(row[0])
+        
+        #self.customerselected = Button(self.custselect, text = "select", command = self._select_customer_account())
+        #self.customerselected.grid(columnspan = 2)
+
+    
+    def _select_customer_account(self):
+        self.custWindow = Toplevel(self.newWindow)
+        self.custWindow.wm_title("Account number #%s" % self.inputmasternum.get())
+        
+
+        
+
+       # for i in range(len(self.mycustomers)):
+            #if self.inputmasternum.get() == self.mycustomers[i]:
+                #custstatement = ('select * from accounts where account_no = ?')
+                #cust = waste.execute(statement, [self.inputmasternum.get()])
+                #self.custl1 = Label(self.newWindow, text = "account number")
+                #self.custl1.grid(row = 0, column = 0)
+               #self.custl2 = Label(self.newWindow, text = "account manager")
+               #self.custl2.grid(row = 0, column = 1)
+               #self.custl3 = Label(self.newWindow, text = "customer name")
+               #self.custl3.grid(row = 0, column = 2)
+               #self.custl4 = Label(self.newWindow, text = "contact info")
+               #self.custl4.grid(row = 0, column = 3)
+               #self.custl5 = Label(self.newWindow, text = "customer type")
+               #self.custl5.grid(row = 0, column = 4)
+               #self.custl6 = Label(self.newWindow, text = "start date")
+               #self.custl6.grid(row = 0, column = 5)
+               #self.custl7 = Label(self.newWindow, text = "end date")
+               #self.custl7.grid(row = 0, column = 6)
+               #self.custl8 = Label(self.newWindow, text = "total amount")
+               #self.custl8.grid(row = 0, column = 7)
+        
+
+
+
 
 root = Tk()
 lf = LoginFrame(root)
